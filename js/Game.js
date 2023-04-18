0efe84e71d7666ccf1d485ac934dba246d8bfac2
yousuf3579 <120466678+yousuf3579@users.noreply.github.com>
2023-04-18 15:55:56 +0400

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,170 @@
+class Game {
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+  }
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 50, height - 100);
+    car1.addImage("car1", car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width / 2 + 100, height - 100);
+    car2.addImage("car2", car2_img);
+    car2.scale = 0.07;
+
+    cars = [car1, car2];
+
+    // C38 TA
+    fuels = new Group();
+    powerCoins = new Group();
+
+    // Adding fuel sprite in the game
+    this.addSprites(fuels, 4, fuelImage, 0.02);
+
+    // Adding coin sprite in the game
+    this.addSprites(powerCoins, 18, powerCoinImage, 0.09);
+  }
+
+  // C38 TA
+  addSprites(spriteGroup, numberOfSprites, spriteImage, scale) {
+    for (var i = 0; i < numberOfSprites; i++) {
+      var x, y;
+
+      x = random(width / 2 + 150, width / 2 - 150);
+      y = random(-height * 4.5, height - 400);
+
+      var sprite = createSprite(x, y);
+      sprite.addImage("sprite", spriteImage);
+
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
+    }
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+    this.resetTitle.html("Reset Game");
+    this.resetTitle.class("resetText");
+    this.resetTitle.position(width / 2 + 200, 40);
+
+    this.resetButton.class("resetButton");
+    this.resetButton.position(width / 2 + 230, 100);
+
+  }
+
+  play() {
+    this.handleElements();
+    this.handleResetButton();
+    Player.getPlayersInfo();
+
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      //index of the array
+      var index = 0;
+      for (var plr in allPlayers) {
+        //add 1 to the index for every loop
+        index = index + 1;
+
+        //use data form the database to display the cars in x and y direction
+        var x = allPlayers[plr].positionX;
+        var y = height - allPlayers[plr].positionY;
+
+        cars[index - 1].position.x = x;
+        cars[index - 1].position.y = y;
+
+        // C38  SA
+        if (index === player.index) {
+          stroke(10);
+          fill("red");
+          ellipse(x, y, 60, 60);
+
+          this.handleFuel(index);
+          this.handlePowerCoins(index);
+          
+          // Changing camera position in y direction
+          camera.position.x = cars[index - 1].position.x;
+          camera.position.y = cars[index - 1].position.y;
+
+        }
+      }
+
+      // handling keyboard events
+      if (keyIsDown(UP_ARROW)) {
+        player.positionY += 10;
+        player.update();
+      }
+      this.handlePlayerControls();
+
+      drawSprites();
+    }
+  }
+
+  handleFuel(index) {
+    // Adding fuel
+    cars[index - 1].overlap(fuels, function(collector, collected) {
+      player.fuel = 185;
+      //collected is the sprite in the group collectibles that triggered
+      //the event
+      collected.remove();
+    });
+  }
+
+  handlePowerCoins(index) {
+    cars[index - 1].overlap(powerCoins, function(collector, collected) {
+      player.score += 21;
+      player.update();
+      //collected is the sprite in the group collectibles that triggered
+      //the event
+      collected.remove();
+    });
+  }
+
+handleResetButton() {
+  this.resetButton.mousePressed(() => {
+    database.ref("/").set({
+      playerCount: 0,
+      gameState: 0,
+      players: {}
+    });
+    window.location.reload();
+  });
+}
+handlePlayerControls() {
+  if (keyIsDown(UP_ARROW)) {
+    player.positionY += 10;
+    player.update();
+  }
+
+  if (keyIsDown(LEFT_ARROW) && player.positionX > width / 3 - 50) {
+    player.positionX -= 5;
+    player.update();
+  }
+
+  if (keyIsDown(RIGHT_ARROW) && player.positionX < width / 2 + 300) {
+    player.positionX += 5;
+    player.update();
+  }
+}
+}
